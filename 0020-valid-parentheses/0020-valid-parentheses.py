e95221e2993c437e143733d760e7108e79c68414
Asmit Maurya <asmitmaurya345@gmail.com>
2026-05-04 22:06:56 +0530

    Time: 217 ms (5.33%), Space: 12.4 MB (69.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isValid(self, s):
+        while "()" in s or "[]" in s or "{}" in s:
+            s = s.replace("()", "").replace("[]", "").replace("{}", "")
+        if s=="":
+            return True
+        else:
+            return False
