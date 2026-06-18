300f9e4cf99226bfae591e7b177801ca5c0fa8a4
Asmit Maurya <asmitmaurya345@gmail.com>
2026-06-18 15:24:43 +0530

    Time: 5 ms (58.91%), Space: 12.3 MB (57.3%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -4,9 +4,7 @@ class Solution(object):
         n2=0
         number=[48,49,50,51,52,53,54,55,56,57]
         for x in num1:
-            n=number.index(ord(x))
-            n1=n1*10+n
+            n1=n1*10+ord(x)-48
         for x in num2:
-            n=number.index(ord(x))
-            n2=n2*10+n
+            n2=n2*10+ord(x)-48
         return str(n1*n2)
