9dde3e7eabaef9e4f3dda8b18dd252eefc5ed90a
Asmit Maurya <asmitmaurya345@gmail.com>
2026-07-05 21:06:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.71%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,10 +1,3 @@
 class Solution(object):
     def checkString(self, s):
-        b=0
-        for x in s:
-            if x=="b":
-                b+=1
-            elif b!=0:
-                return False
-        else:
-            return True
+        return "ba" not in s
