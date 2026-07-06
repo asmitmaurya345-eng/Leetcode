af6c83cabdbb0719aa37584a7f4165746ea340b0
Asmit Maurya <asmitmaurya345@gmail.com>
2026-07-06 22:59:46 +0530

    Time: 5 ms (31.82%), Space: 12.4 MB (53.66%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -2,17 +2,32 @@ class Solution(object):
     def mySqrt(self, x):
         if x<2:
             return x
-        l=1
-        r=x
-        ans=0
-        while l<=r:
-            m=(l+r)//2
-            n=m*m
-            if n==x:
-                return m
-            elif x<n:
-                r=m-1
-            else:
-                l=m+1
-                ans=m
+        elif x==2 or x==3:
+            l=1
+            r=x
+            ans=0
+            while l<=r:
+                m=(l+r)//2
+                n=m*m
+                if n==x:
+                    return m
+                elif x<n:
+                    r=m-1
+                else:
+                    l=m+1
+                    ans=m
+        else:
+            l=2
+            r=x//2
+            ans=0
+            while l<=r:
+                m=(l+r)//2
+                n=m*m
+                if n==x:
+                    return m
+                elif x<n:
+                    r=m-1
+                else:
+                    l=m+1
+                    ans=m
         return ans
