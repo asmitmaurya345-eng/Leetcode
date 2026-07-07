58640cc1cd2a42da52b382503c50adb2a825beff
Asmit Maurya <asmitmaurya345@gmail.com>
2026-07-07 16:37:32 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        n=n+1
-        for x in range(-1,-n,-1):
+        for x in range(-1,-n-1,-1):
             nums1[x]=nums2[x]
         nums1.sort()
