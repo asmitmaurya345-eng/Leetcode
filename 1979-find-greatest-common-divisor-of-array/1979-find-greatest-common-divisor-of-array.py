7b10b8b13b7601ae2c5ccb8a5272202722b515b2
Asmit Maurya <asmitmaurya345@gmail.com>
2026-07-18 22:19:36 +0530

    Time: 3 ms (22.89%), Space: 12.7 MB (0.96%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -1,7 +1,5 @@
 import fractions
 class Solution(object):
     def findGCD(self, nums):
-        n1=min(nums)
-        n2=max(nums)
-        return fractions.gcd(n1,n2)
+        return fractions.gcd(min(nums),max(nums))
         
