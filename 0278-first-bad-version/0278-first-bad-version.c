24bc648a0c8e577a3afb07c640ba2813121c9384
Asmit Maurya <asmitmaurya345@gmail.com>
2026-08-16 14:54:23 +0530

    Time: 3 ms (55.78%), Space: 8.5 MB (29.4%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.c
+++ 0278-first-bad-version/0278-first-bad-version.c
@@ -1,12 +1,12 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-int firstBadVersion(long int n) 
+int firstBadVersion(int n) 
 {
-    long int l=1;
+    int l=1;
     while (l<n)
     {
-        long int m=(l+n)/2;
+        int m=l+(n-l)/2;
         if (isBadVersion(m))
         {
             n=m;
